db4a5a58d46862c2a8b942007bb8cb359262a96d
Joseph Karasek and Jordan Meier <joseph.karasek@gmail.com>
2016-02-15 09:57:18 -0800

    add form and result html.twig

--- app/app.php
+++ app/app.php
@@ -9,7 +9,15 @@
     ));
 
     $app->get("/", function() use ($app){
-        return $app['twig']->('form.html.twig');
+        return $app['twig']->render('form.html.twig');
+    });
+
+    $app->get("/ping_pong", function() use ($app){
+        $input = $_GET['number'];
+        $my_PingPongGenerator = new PingPongGenerator;
+        $results = $my_PingPongGenerator->generatePingPongArray($input);
+
+        return $app['twig']->render('results.html.twig', array('results' => $results));
     });
 
     return $app;
