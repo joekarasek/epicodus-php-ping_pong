7b37bf20e899907c5ffa419f065ab03a14308b8a
Joseph Karasek and Jordan Meier <joseph.karasek@gmail.com>
2016-02-15 09:47:23 -0800

    add a home route and a form page in twig

--- app/app.php
+++ app/app.php
@@ -1,10 +1,15 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/PingPongGenerator.php";
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Home";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app){
+        return $app['twig']->('form.html.twig');
     });
 
     return $app;
